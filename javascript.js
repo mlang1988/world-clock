cdcaa9fdf36433b4f3925d8c17e6aa02836b46e8
mlang1988 <165694522+mlang1988@users.noreply.github.com>
2024-04-20 18:07:12 +0200

    Added javascripst to cities

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format("HH:mm:ss");
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format("HH:mm:ss");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
